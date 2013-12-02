f8a743ced568049bb3a82ccf90a6545be44d3384
npower <npower@mit.edu>
2013-12-02 00:55:40 -0500

    finished main

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
 import nussinov
 import fold
-import timeit
 import parse
 import matplotlib
 
 
 def mainPartitioned(partition_length):
-    seqList = parse.parse(r'C:\Users\LLP-student\Documents\GitHub\rna-folding\RNAseqs')
+    seqList = parse.parse(r'C:\Users\LLP-student\Documents\GitHub\rna-folding\RNAseqs', 20)
     folded = []
     for RNA in seqList:
         folded += [fold.Fold(RNA, partition_length)] #with partition, run with different lengths
     return folded
 
 def mainNoPartition():
-    seqList = parse.parse(r'C:\Users\LLP-student\Documents\GitHub\rna-folding\RNAseqs')
+    seqList = parse.parse(r'C:\Users\LLP-student\Documents\GitHub\rna-folding\RNAseqs', 20)
     scoredFolded = []
     for RNA in seqList:
-        scoredFolded += [nussinov.FoldAndScore(RNA)]      #without partition
+        scoredFolded += [nussinov.FoldAndScore(RNA)] #without partition
     return scoredFolded
 
 def scorePartitioned(folded):
     pass
 
 
-mainNoPartition()
+
+
+if __name__ == '__main__':
+    import timeit
+    t = timeit.Timer(stmt = "mainNoPartition()", setup = "from main import mainNoPartition")
+    u = timeit.Timer(stmt = "mainPartitioned(20)", setup = "from main import mainPartitioned")
+    print t.timeit(3)
+    print u.timeit(3)
--- parse.py
+++ parse.py
@@ -8,7 +8,7 @@ Takes in a path to a directory, then opens each file in the
 directory (presumed to be a in .dp format) and parses out the RNA
 sequence. Returns a list of all RNA sequences as strings.
 """
-def parse(path):
+def parse(path, numRNA):
     ##finds all files in directory
     data = []
     for dir_entry in os.listdir(path):
@@ -18,7 +18,7 @@ def parse(path):
 
     ##opens each file and parses for RNA sequence
     seqList = []
-    for theFile in data:
+    for theFile in data[:numRNA]:
         h = open(theFile)
         k = 0
         seq = []
