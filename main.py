e9e199fcb53ff412f5955caae67d0459741995a2
npower <npower@mit.edu>
2013-12-02 21:22:53 -0500

    changed number of sequences variable to an arg of main functions

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ import fold
 import parse
 import matplotlib
 
-def mainPartitioned(partition_length):
-    seqList = parse.parse('RNAseqs', 1)
+def mainPartitioned(numSeqs, partition_length):
+    seqList = parse.parse('RNAseqs', numSeqs)
     folded = []
     for RNA in seqList:
         if len(RNA) != 0:
           folded += [fold.Fold(RNA, partition_length)] #with partition, run with different lengths
     return folded
 
-def mainNoPartition():
-    seqList = parse.parse('RNAseqs', 1)
+def mainNoPartition(numSeqs):
+    seqList = parse.parse('RNAseqs', numSeqs)
     scoredFolded = []
     for RNA in seqList:
         if len(RNA) != 0:
@@ -27,7 +27,7 @@ def scorePartitioned(folded):
 
 if __name__ == '__main__':
     import timeit
-    t = timeit.Timer(stmt = "mainNoPartition()", setup = "from main import mainNoPartition")
-    u = timeit.Timer(stmt = "mainPartitioned(10)", setup = "from main import mainPartitioned")
+    t = timeit.Timer(stmt = "mainNoPartition(20)", setup = "from main import mainNoPartition")
+    u = timeit.Timer(stmt = "mainPartitioned(20, 10)", setup = "from main import mainPartitioned")
     print 'Without partioning:', t.timeit(3)
     print 'With partitioning:', u.timeit(3)
