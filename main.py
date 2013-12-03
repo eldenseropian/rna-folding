24fc654d99109d4fd74f5e0a40269ea56af1360e
Lily Seropian <lilyseropian@gmail.com>
2013-12-02 23:21:20 -0500

    Added check for length of RNA and labels to timeout output

--- main.py
+++ main.py
@@ -8,14 +8,16 @@ def mainPartitioned(partition_length):
     seqList = parse.parse('RNAseqs', 20)
     folded = []
     for RNA in seqList:
-        folded += [fold.Fold(RNA, partition_length)] #with partition, run with different lengths
+        if len(RNA) != 0:
+          folded += [fold.Fold(RNA, partition_length)] #with partition, run with different lengths
     return folded
 
 def mainNoPartition():
     seqList = parse.parse('RNAseqs', 20)
     scoredFolded = []
     for RNA in seqList:
-        scoredFolded += [nussinov.FoldAndScore(RNA)] #without partition
+        if len(RNA) != 0:
+          scoredFolded += [nussinov.FoldAndScore(RNA)] #without partition
     return scoredFolded
 
 def scorePartitioned(folded):
@@ -28,5 +30,5 @@ if __name__ == '__main__':
     import timeit
     t = timeit.Timer(stmt = "mainNoPartition()", setup = "from main import mainNoPartition")
     u = timeit.Timer(stmt = "mainPartitioned(20)", setup = "from main import mainPartitioned")
-    print t.timeit(3)
-    print u.timeit(3)
+    print 'Without partioning:', t.timeit(3)
+    print 'Partitioned:', u.timeit(3)
