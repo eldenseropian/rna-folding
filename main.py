89984361b1dfbd99b4b80fd31df9411ab4e73376
npower <npower@mit.edu>
2013-12-06 02:16:48 -0800

    csvs and csv writer in main

--- main.py
+++ main.py
@@ -2,38 +2,51 @@ import nussinov
 import fold
 import parse
 import pairingScorer
-import matplotlib
+import csv
+##import matplotlib
 
+'''
+both mainPartioned and mainNoPartiition return a tuple (score, fold)
+where score is an integer and fold is a list of tuples of tuples
+ex: [(tuple base1, tuple base2),...,(tuple baseX, tuple baseY)]
+where each base-tuple is (string letter, int position)
+altogether, something of the form (1,[((b,2),(d,3)),...,((f,4),(h,5))])
+'''
 def mainPartitioned(numSeqs, partition_length):
-    seqList = parse.parse('RNAseqs', numSeqs)
-    folded = []
-    for RNA in seqList:
-        if len(RNA) != 0:
-          folded += [fold.Fold(RNA, partition_length)] #with partition, run with different lengths
-    return folded
+    totalScore = 0
+    with open('partitioned.csv', 'a') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        seqList = parse.parse('RNAseqs', numSeqs)
+        scoredFolded = []
+        for RNA in seqList:
+            if len(RNA) != 0:
+                x = fold.Fold(RNA, partition_length)
+                totalScore += x[0]
+                scoredFolded += [x]
+        aveScore = totalScore/float(numSeqs)
+        writer.writerow([partition_length, aveScore])
+    return scoredFolded
 
 def mainNoPartition(numSeqs):
     seqList = parse.parse('RNAseqs', numSeqs)
     scoredFolded = []
     for RNA in seqList:
         if len(RNA) != 0:
-          scoredFolded += [nussinov.FoldAndScore(nussinov.MakeSeq(RNA))] #without partition
+          scoredFolded += [nussinov.FoldAndScore(nussinov.MakeSeq(RNA))] 
     return scoredFolded
 
-def scorePartitioned(folded):
-    return pairingScorer.score(folded)
 
+##[mainPartitioned(4500, p) for p in range (1, 30, 1)]
 
-
-
-if __name__ == '__main__':
-    import timeit
-    sequences = 1
-    t = timeit.Timer(stmt = "mainNoPartition(" + str(sequences) + ")", setup = "from main import mainNoPartition")
-    u = timeit.Timer(stmt = "mainPartitioned(" + str(sequences) + ", " + str(100) + ")", setup = "from main import mainPartitioned")
-    tests = [timeit.Timer(stmt = "mainPartitioned(" + str(sequences) + ", " + str(p) + ")", setup = "from main import mainPartitioned") for p in range(150, 250, 20)]
-    trials = 5
-    print 'Without partioning:', min([t.timeit(1) for x in xrange(1)])
+##if __name__ == '__main__':
+##    import timeit
+##    sequences = 1
+##    t = timeit.Timer(stmt = "mainNoPartition(" + str(sequences) + ")", setup = "from main import mainNoPartition")
+##    u = timeit.Timer(stmt = "mainPartitioned(" + str(sequences) + ", " + str(100) + ")", setup = "from main import mainPartitioned")
+##    tests = [timeit.Timer(stmt = "mainPartitioned(" + str(sequences) + ", " + str(p) + ")", setup = "from main import mainPartitioned") for p in range(4, 50, 2)]
+##    trials = 5
+##    print 'Without partitioning:', min([t.timeit(1) for x in xrange(1)])
 ##    print 'With threading:', min([u.timeit(1) for x in xrange(trials)])
-    for num, test in enumerate(tests):
-        print 'Partition size ' + str(150 + 20*num) + ': ', test.timeit(trials)
+##    for num, test in enumerate(tests):
+##        print 'Partition size ' + str(4 + 2*num) + ': ', test.timeit(trials)
